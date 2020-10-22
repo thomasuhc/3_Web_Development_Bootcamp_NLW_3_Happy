1499b5351a43749d4016d5a9f53d21acd96ecaac
Thomas Chu <thomasuhc@gmail.com>
2020-10-22 13:36:58 -0400

    Completed Animate page orphanage
    Animate page orphanage and Z index

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,25 @@
+const map = L.map('mapid').setView([-23.1935807,-45.8876018], 15);
+
+L
+.tileLayer
+('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', )
+.addTo(map);
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+const popup = L.popup({
+    closeButton: false,
+    ClassName: "map-popup",
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>');
+
+L
+.marker([-23.1935807,-45.8876018], {icon})
+.addTo(map)
+.bindPopup(popup)

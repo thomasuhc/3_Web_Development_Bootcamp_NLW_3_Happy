cff16cd7829d19fddafb58ffa37bbed7e5281145
Thomas Chu <thomasuhc@gmail.com>
2020-10-25 02:56:30 -0400

    Completed Phase 04 Bootcamp
    Phase 04 Bootcamp

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -6,7 +6,7 @@ L
 .addTo(map);
 
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68]
 
@@ -17,7 +17,7 @@ const popup = L.popup({
     ClassName: "map-popup",
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>');
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg" </a>');
 
 
 let marker;
---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-23.1935807",
+        lng: "-45.8876018",
+        name: "Lar das criancas",
+        description: "Presta assistencia a criancas de 06 a 15 anos que ser encontre em situacao de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=723&q=80",
+
+            "https://images.unsplash.com/photo-1533222481259-ce20eda1e20b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar.",
+        opening_hours: "Horario de visitas das 8h ate 18h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        lat: "-23.194981",
+        lng: "-45.8924977",
+        name: "Lar das alunas",
+        description: "Presta assistencia a criancas de 06 a 15 anos que ser encontre em situacao de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=723&q=80",
+
+            "https://images.unsplash.com/photo-1533222481259-ce20eda1e20b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar.",
+        opening_hours: "Horario de visitas das 8h ate 18h",
+        open_on_weekends: "0"
+    }
+]
